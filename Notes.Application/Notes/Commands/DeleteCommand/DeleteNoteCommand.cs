4366c8bca08681ac2f60bc35833177bd29eba875
Alex Newman <bakaev_a1998@mail.ru>
2022-08-08 12:22:40 +0300

    Implemented delete note command

---
+++ Notes.Application/Notes/Commands/DeleteCommand/DeleteNoteCommand.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+
+namespace Notes.Application.Notes.Commands.DeleteCommand
+{
+    /// <summary>
+    /// Delete note command
+    /// </summary>
+    public class DeleteNoteCommand : IRequest
+    {
+        /// <summary>
+        /// User id
+        /// </summary>
+        public Guid UserId { get; set; }
+
+        /// <summary>
+        /// Note id
+        /// </summary>
+        public Guid Id { get; set; }
+    }
+}
---
+++ Notes.Application/Notes/Commands/DeleteCommand/DeleteNoteCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using Notes.Application.Common.Exceptions;
+using Notes.Application.Interfaces;
+using Notes.Domain;
+
+namespace Notes.Application.Notes.Commands.DeleteCommand
+{
+    public class DeleteNoteCommandHandler
+        : IRequestHandler<DeleteNoteCommand>
+    {
+        /// <summary>
+        /// Db context
+        /// </summary>
+        private readonly INotesDbContext _dbContext;
+
+        /// <summary>
+        /// Initializes class instance of <see cref="DeleteNoteCommandHandler"/>
+        /// </summary>
+        /// <param name="dbContext">Db context</param>
+        public DeleteNoteCommandHandler(INotesDbContext dbContext) =>
+            _dbContext = dbContext;
+
+        public async Task<Unit> Handle(DeleteNoteCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _dbContext.Notes
+                .FindAsync(new object[] { request.Id }, cancellationToken);
+
+            if (entity == null || entity.UserId != request.UserId)
+            {
+                throw new NotFoundException(nameof(Note), request.Id);
+            }
+
+            _dbContext.Notes.Remove(entity);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
