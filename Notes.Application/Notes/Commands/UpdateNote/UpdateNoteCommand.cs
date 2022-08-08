25df600cf1467fef4b8ccaa1d34fd4977a1a4617
Alex Newman <bakaev_a1998@mail.ru>
2022-08-08 12:23:15 +0300

    Implemented Update note command

---
+++ Notes.Application/Notes/Commands/UpdateNote/UpdateNoteCommand.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+
+namespace Notes.Application.Notes.Commands.UpdateNote
+{
+    public class UpdateNoteCommand : IRequest
+    {
+        /// <summary>
+        /// User id
+        /// </summary>
+        public Guid UserId { get; set; }
+
+        /// <summary>
+        /// Note id
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Note title
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Note details
+        /// </summary>
+        public string Details { get; set; }
+    }
+}
---
+++ Notes.Application/Notes/Commands/UpdateNote/UpdateNoteCommandHandler.cs
@@ -0,0 +1,50 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Notes.Application.Common.Exceptions;
+using Notes.Application.Interfaces;
+using Notes.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Application.Notes.Commands.UpdateNote
+{
+    public class UpdateNoteCommandHandler
+        : IRequestHandler<UpdateNoteCommand>
+    {
+        /// <summary>
+        /// Db context
+        /// </summary>
+        private readonly INotesDbContext _dbContext;
+
+        /// <summary>
+        /// Initializes class instance of <see cref="UpdateNoteCommandHandler"/>
+        /// </summary>
+        /// <param name="dbContext">Db context</param>
+        public UpdateNoteCommandHandler(INotesDbContext dbContext) =>
+            _dbContext = dbContext;
+
+        public async Task<Unit> Handle(UpdateNoteCommand request, 
+            CancellationToken cancellationToken)
+        {
+            var entity =
+                await _dbContext.Notes.FirstOrDefaultAsync(note 
+                    => note.Id == request.Id, cancellationToken);
+
+            if (entity == null || entity.UserId != request.UserId)
+            {
+                throw new NotFoundException(nameof(Note), request.Id);
+            }
+
+            entity.Details = request.Details;
+            entity.Title = request.Title;
+            entity.EditDate = DateTime.Now;
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
