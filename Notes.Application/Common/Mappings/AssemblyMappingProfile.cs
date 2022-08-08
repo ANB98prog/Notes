88844e9274f3a4855a7bda9672024034996a334f
Alex Newman <bakaev_a1998@mail.ru>
2022-08-08 09:20:43 +0300

    Added automapper configuration to project

---
+++ Notes.Application/Common/Mappings/AssemblyMappingProfile.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using System.Reflection;
+
+namespace Notes.Application.Common.Mappings
+{
+    /// <summary>
+    /// Assembles mapping profiles
+    /// </summary>
+    public class AssemblyMappingProfile : Profile
+    {
+        public AssemblyMappingProfile(Assembly assembly) =>
+            ApplyMappingsFromAssembly(assembly);
+
+        /// <summary>
+        /// Applies mapping from assembly
+        /// </summary>
+        /// <param name="assembly">Assembly</param>
+        private void ApplyMappingsFromAssembly(Assembly assembly)
+        {
+            var types = assembly.GetExportedTypes()
+                            .Where(type => type.GetInterfaces()
+                                .Any(i => i.IsGenericType 
+                                    && i.GetGenericTypeDefinition() == typeof(IMapWith<>)))
+                            .ToList();
+
+            foreach (var type in types)
+            {
+                var instance = Activator.CreateInstance(type);
+                var methodInfo = type.GetMethod("Mapping");
+                methodInfo?.Invoke(instance, new object[] { this });
+            }
+        }
+    }
+}
---
+++ Notes.Application/Common/Mappings/IMapWith.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+namespace Notes.Application.Common.Mappings
+{
+    /// <summary>
+    /// Interface of mapping configuration
+    /// </summary>
+    /// <typeparam name="T">Mapper type</typeparam>
+    public interface IMapWith<T>
+    {
+        void Mapping(Profile profile) =>
+             profile.CreateMap(typeof(T), GetType());
+    }
+}
