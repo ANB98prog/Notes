83197177021a6ca1583c564c6852d54812c71a91
Alex Newman <bakaev_a1998@mail.ru>
2022-08-30 16:24:58 +0300

    Added simple frontend

---
+++ notes.frontend/src/auth/SigninOidc.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useEffect, FC } from 'react';
+import { useHistory } from 'react-router-dom';
+import { signinRedirectCallback } from './user-service';
+
+const SigninOidc: FC<{}> = () => {
+    const history = useHistory();
+    useEffect(() => {
+        async function signinAsync() {
+            await signinRedirectCallback();
+            history.push('/');
+        }
+        signinAsync();
+    }, [history]);
+
+    return <div>Redirecting...</div>;
+};
+
+export default SigninOidc;
---
+++ notes.frontend/src/auth/SignoutOidc.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { FC, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { signoutRedirectCallback } from './user-service';
+
+
+const SignoutOidc: FC<{}> = () => {
+    const history = useHistory();
+    useEffect(() => {
+        const signoutAsync = async () =>
+        {
+            await signoutRedirectCallback();
+            history.push('/');
+        };
+
+        signoutAsync();
+    }, [history]);
+
+    return <div>Redirecting...</div>;
+}; 
+
+export default SignoutOidc;
